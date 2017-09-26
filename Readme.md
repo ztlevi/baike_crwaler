4f3ea6cf1c3335b490208b4dbb9f216e153902fc
Ting Zhou <zhouting@umich.edu>
2017-09-25 20:44:16 -0400

    Init

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Baike Crawler
+
+This is the crawler for Baidu's baike.
---
+++ baike_spider/spider_main.py
@@ -0,0 +1,36 @@
+import html_downloader, html_outputer, html_parser, url_manager
+
+
+class SpiderMain(object):
+    def __init__(self):
+        self.urls = url_manager.UrlManager()
+        self.downloader = html_downloader.HtmlDownloader()
+        self.parser = html_parser.HtmlParser()
+        self.outputer = html_outputer.HtmlOutputer()
+
+    def craw(self, root_url):
+        self.urls.add_new_url(root_url)
+        count = 0
+        while self.urls.has_new_url():
+            try:
+                new_url = self.urls.get_new_url()
+                print 'craw %d : %s' % (count, new_url)
+                html_cont = self.downloader.download(new_url)
+                new_urls, new_data = self.parser.parse(new_url, html_cont)
+                self.urls.add_new_urls(new_urls)
+                self.outputer.collect_data(new_data)
+
+                if count == 1000:
+                    break
+
+                count += 1
+            except:
+                print 'craw failed'
+
+        self.outputer.output_html()
+
+
+if __name__ == '__main__':
+    root_url = 'https://baike.baidu.com/item/Python'
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)
